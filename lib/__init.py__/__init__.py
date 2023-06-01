cf94ffe3b6d53cc263d220852e7728cec87644f0
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-06-02 00:51:53 +0300

    push

--- lib/__init.py__/__init__.py
+++ lib/__init.py__/__init__.py
@@ -1,1 +1,14 @@
+# __init__.py
 
+# from .module1 import SomeClass
+# from .subpackage import submodule
+
+PACKAGE_NAME = "mypackage"
+VERSION = "1.0.0"
+
+# Initialization logic
+print("Initializing mypackage...")
+
+# Additional code or configurations
+
+print("mypackage initialization complete.")
